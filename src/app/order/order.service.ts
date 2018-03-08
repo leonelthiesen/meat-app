545fe8ec34f3c225dc8f5e24e0997ba6970ffc27
unknown <leonelthiesen@gmail.com>
2018-03-07 21:45:57 -0300

    Utilizando interceptor para enviar autorização.

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -5,11 +5,10 @@ import { Order } from './order.model';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MEAT_API } from '../app.api';
-import { LoginService } from '../security/login/login.service';
 
 @Injectable()
 export class OrderService {
-    constructor(private cartService: ShoppingCartService, private http: HttpClient, private loginService: LoginService) {}
+    constructor(private cartService: ShoppingCartService, private http: HttpClient) {}
 
     itemsValue(): number {
         return this.cartService.total();
@@ -36,12 +35,7 @@ export class OrderService {
     }
 
     checkOrder(order: Order): Observable<string> {
-        let headers = new HttpHeaders();
-
-        if (this.loginService.isLoggedIn()) {
-            headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`);
-        }
-        return this.http.post<Order>(`${MEAT_API}/orders`, order, { headers: headers })
+        return this.http.post<Order>(`${MEAT_API}/orders`, order)
             .map(response => response.id);
     }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -13,6 +13,8 @@ import { NotificationService } from './messages/notification.service';
 import { LoginService } from '../security/login/login.service';
 import { LoggedInGuard } from '../security/loggedin.guard';
 import { LeaveOrderGuard } from '../order/leave-order.guard';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from '../security/auth.interceptor';
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -29,7 +31,8 @@ export class SharedModule {
                         NotificationService,
                         LoginService,
                         LoggedInGuard,
-                        LeaveOrderGuard]
+                        LeaveOrderGuard,
+                        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }]
         };
     }
 }
